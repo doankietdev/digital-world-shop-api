9ba930d85c2ecb7110857d5fb04dad05566b5ed7
doankietdev <doankietdev@gmail.com>
2024-04-05 18:58:08 +0700

    edit white list domain cors; fix categories api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const DEV_ENV = 'development'
 
 export const PROD_ENV = 'production'
 
-export const WHITE_LIST_DOMAINS = ['http://localhost']
+export const WHITE_LIST_DOMAINS = ['http://localhost:3000']
 
 export const HEADER_KEYS = {
   USER_ID: 'x-user-id',
