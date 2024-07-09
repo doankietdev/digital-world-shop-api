dcccb66f247e227447b900c8d9d0c80590679b24
doankietdev <doankietdev@gmail.com>
2024-07-09 10:54:06 +0700

    [EDIT] fix create paypal order

--- src/services/checkoutService.js
+++ src/services/checkoutService.js
@@ -274,8 +274,7 @@ const createPayPalOrder = async (userId, reqBody) => {
           },
           shipping: {
             currency_code: 'USD',
-            // value: Math.round(shippingFee / 23500)
-            value: shippingFee
+            value: Math.round(shippingFee / 23500)
           }
         }
       }
