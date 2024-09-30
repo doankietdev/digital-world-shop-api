c5e940f7ced4902fbb8731dd247b0391ad39d043
LamNoOne <trannguyendaclam@gmail.com>
2024-10-01 00:48:01 +0700

    update exit hook flushall redis - close db

--- server.js
+++ server.js
@@ -1,36 +1,50 @@
 /* eslint-disable no-console */
-import exitHook from 'async-exit-hook'
-import app from '~/app'
-import { connectDB, closeDB } from '~/configs/mongodb'
-import { APP } from '~/configs/environment'
-import { RedisDB } from '~/databases/init.redis'
+import exitHook from "async-exit-hook"
+import app from "~/app"
+import { connectDB, closeDB } from "~/configs/mongodb"
+import { APP } from "~/configs/environment"
+import { RedisDB } from "~/databases/init.redis"
 
 const startServer = async () => {
   const server = app.listen(APP.PORT, APP.HOST, () => {
     console.log(`Server is running at ${APP.HOST}:${APP.PORT}`)
   })
 
-  exitHook(() => {
-    closeDB()
-      .then(() => {
-        console.log('Disconnected from MongoDB successfully')
-        server.close()
-        console.log('Closed server')
+  const redisClient = RedisDB.getInstance().getRedis()
+
+  exitHook((callback) => {
+    Promise.all([
+      closeDB()
+        .then(() => {
+          console.log("Disconnected from MongoDB successfully")
+        })
+        .catch(() => {
+          console.log("Disconnect from MongoDB failed")
+        }),
+      redisClient
+        .flushall()
+        .then(() => {
+          console.log("Flushed all Redis cache successfully")
+        })
+        .catch((err) => {
+          console.log("Failed to flush Redis cache", err)
+        }),
+    ]).finally(() => {
+      server.close(() => {
+        console.log("Closed server")
+        callback()
       })
-      .catch(() => {
-        console.log('Disconnect from MongoDB failed')
-      })
+    })
   })
 }
 
-(async () => {
+;(async () => {
   try {
-    RedisDB.getInstance()
     await connectDB()
-    console.log('Connect to MongoDB successfully')
+    console.log("Connect to MongoDB successfully")
     startServer()
   } catch (error) {
-    console.log('Connect to MongoDB failed')
+    console.log("Connect to MongoDB failed")
     process.exit(0)
   }
 })()
